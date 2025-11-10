167b6a5a598b20cb0b68e564cb6099d0e3b05243
JuuMarkman28 <jmarkmanurena@udesa.edu.ar>
2025-11-10 11:17:58 -0300

    Adelanto del punto cinco, product.js y actualizacion de los html

--- js/product.js
+++ js/product.js
@@ -1,6 +1,8 @@
 let qs = location.search
 
 let qsObj = new URLSearchParams(qs)
+console.log(qsObj)
+
 
 let idBuscado = qsObj.get("idProduct")
 
@@ -11,22 +13,52 @@ let seccion = document.querySelector(".seccion_cuerpo");
 
 
 fetch(URL)
-.then(function(response) {
-    return response.json()
-})
-.then(function(data) {
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
 
-    let contenido = ""
-    const element = data
+        let contenido = ""
+        const element = data
 
-    contenido += ``
-        
-
-    seccion.innerHTML = contenido
-
+        contenido += `<article class="titulo img_product">
+                    <img class="img_detalle" src="${element.images[0]}" alt="galletitas">
+                </article>
 
 
-})
-.catch(function(error) {
-    console.log("Error: " + error);
-})
+                <article class="art_subt">
+                    <p class="subt_product">Marca: </p>
+                    <p class="p_product">${element.brand}</p>
+                </article>
+
+                <article class="art_desc_product">
+                    <p>${element.description}</p>
+                </article>
+
+                <article class="art_subt">
+                    <p class="subt_product">Precio: </p>
+                    <p class="p_product">$ ${element.price}</p>
+                </article>
+
+                <article class="art_subt">
+                    <p class="subt_product">Categoria: </p>
+                    <p class="p_product">${element.category}</p>
+                </article>
+
+                <article class="art_subt">
+                    <p class="subt_product">Stock: </p>
+                    <p class="p_product">${element.stock} unidades</p>
+                </article>
+
+                <article class="art_subt">
+                    <p class="subt_product">Tags: </p>
+                    <p class="p_product">${element.tags}</p>
+                </article>`
+
+
+        seccion.innerHTML += contenido
+
+    })
+    .catch(function (error) {
+        console.log("Error: " + error);
+    })
