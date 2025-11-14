e4be34acd4a1383416ce5d3de0002d472cf2bba6
matepena06-coder <matepena06@gmail.com>
2025-11-14 16:39:09 -0300

    Avance con search results

--- js/search-results.js
+++ js/search-results.js
@@ -1,12 +1,45 @@
-fetch()
-.then(function(response){
-    return response.json();
-})
+let qs = location.search
+let qsObj = new URLSearchParams(qs)
 
-.then(function(data){
+let idBuscado = qsObj.get("buscar")
+let palabraBuscadoForm = qsObj.get("buscar")
+let seccion = document.querySelector(".seccion-cuerpo");
 
-})
+let URL = `https://dummyjson.com/products/?name=${palabraBuscadoForm}`;
 
-.catch(function(error){
-    alert("No hay resultados para el término: ")
-})
+fetch(URL)
+
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (data) {
+
+        let contenido = ""
+    
+        if (data.products.length >= 1) {
+            for (let i = 0; i < data.products.length; i++) {
+                const element = data.products[i]
+                contenido += `
+                <article class="article_productos">
+                    <h4>${element.title}</h4>
+                    <img src="${element.images[0]}" alt="">
+                    <p class="descrip_prod">${element.description}</p>
+                    <p class="precio">$ ${element.price}</p>
+                    <a class="verDetalle" href="./product.html?idProduct=${element.id}">Ver detalle</a>
+                </article>`
+
+            }
+        } else {
+            
+            contenido += `
+        <article>
+            <p>No hay resultados para el término: ${palabraBuscadoForm}</p>
+        </article>`
+        }
+    })
+
+    .catch(function (error) {
+        
+        console.log("Error: " + error)
+    })
