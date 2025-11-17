adb99339ec537dcdc7ef5a0dc56d105cbad26c7e
JuuMarkman28 <jmarkmanurena@udesa.edu.ar>
2025-11-17 16:08:56 -0300

    pequeña modificacion de la seccion de producto

--- js/register.js
+++ js/register.js
@@ -4,12 +4,14 @@ let texto_oculto = document.querySelector(".textooculto")
 let texto_oculto2 = document.querySelector(".textooculto2")
 let formulario = document.querySelector(".formularioRegistro")
 let contrasenia2 = document.querySelector(".textocontraseña2")
+let terminos = document.querySelector(".terminos")
 
 formulario.addEventListener("submit", function (e) {
     e.preventDefault()
 
     let longitudContrasenia = contrasenia.value.length
     let longitudMail = mail.value.length
+    let condicion = terminos.value
 
     if (longitudMail == 0) {
         alert("Rellenar el campo de email, por favor")
@@ -26,7 +28,7 @@ formulario.addEventListener("submit", function (e) {
             texto_oculto.style.display = "none"
             texto_oculto2.style.display = "block"
 
-        } else {
+        } else { 
             formulario.submit()
         }
     }
